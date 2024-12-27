d01057d7f84f3c433dc71e8b4dbdd065bcbb0c33
Samuel Cristobal <samuel.cristobal@protonmail.com>
2024-12-27 21:44:03 +0100

    Add PostHog config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import { ClerkProvider } from '@clerk/nextjs';
+import { CSPostHogProvider } from './providers'
+
+
 import './globals.css';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
+    return <CSPostHogProvider>
         <ClerkProvider>
+
             <html lang="en">
                 <body>{children}</body>
             </html>
-        </ClerkProvider>
-    );
+        </ClerkProvider></CSPostHogProvider>
 }
---
+++ app/providers.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+import { PropsWithChildren } from 'react';
+
+if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY && process.env.NEXT_PUBLIC_POSTHOG_HOST) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
+  })
+}
+export function CSPostHogProvider({ children }: PropsWithChildren) {
+    return <PostHogProvider client={posthog}>{children}</PostHogProvider>
+}
