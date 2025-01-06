3704fcf7236851b500c338dded219d36c0cf5ed9
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-01-06 11:14:16 +0100

    Fix missing deserialization

--- lib/db/queries.ts
+++ lib/db/queries.ts
@@ -6,22 +6,22 @@ import { Scenario, scenarioSchema } from '~/lib/domain/scenario';
 export const getScenarios = async () => {
     const res = await db.query.ScenariosTable.findMany();
 
-    return res.map((row) => ({ ...row, data: scenarioSchema.parse(row.data) }));
+    return res.map((row) => ({ ...row, data: scenarioSchema.parse(JSON.parse(row.data)) }));
 };
 
 export const getScenario = async (id: number) => {
     const res = await db.query.ScenariosTable.findFirst({ where: (scenario, { eq }) => eq(scenario.id, id) });
-    return res ? { ...res, data: scenarioSchema.parse(res.data) } : res;
+    return res ? { ...res, data: scenarioSchema.parse(JSON.parse(res.data)) } : res;
 };
 
 export const writeScenario = async (scenario: Scenario) => {
     const data = JSON.stringify(scenario);
     const res = await db.insert(ScenariosTable).values({ data }).returning();
 
-    return res.map((row) => ({ ...row, data: scenarioSchema.parse(row.data) }));
+    return res.map((row) => ({ ...row, data: scenarioSchema.parse(JSON.parse(row.data)) }));
 };
 
 export const deleteScenario = async (id: number) => {
     const res = await db.delete(ScenariosTable).where(eq(ScenariosTable.id, id)).returning();
-    return res.map((row) => ({ ...row, data: scenarioSchema.parse(row.data) }));
+    return res.map((row) => ({ ...row, data: scenarioSchema.parse(JSON.parse(row.data)) }));
 };
