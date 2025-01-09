6e267884c61cbc049f896807381838d9be70fb42
David González <dg@databeacon.aero>
2025-01-09 18:09:33 +0100

    Simplify using type coallesce

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,12 @@ import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import { Button } from '~/components/ui/button';
 import { createOrUpdateUser } from '~/lib/db/queries';
-import { User } from '~/lib/domain/user';
 
 export default async function Page() {
     const user = await currentUser();
 
     if (user) {
-        const userData: User = {
-            id: user.id,
-            firstName: user.firstName,
-            lastName: user.lastName
-        };
-
-        createOrUpdateUser(userData);
+        createOrUpdateUser(user);
         redirect('/play/random');
     }
 
