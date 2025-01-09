9eb332a49cc2c5a82a552e1b34fd34ef31c8154e
David González <dg@databeacon.aero>
2025-01-09 12:54:15 +0100

    Apply feedback on not-found style

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,17 +1,14 @@
-'use client';
-
 import Link from 'next/link';
+import { Button } from '~/components/ui/button';
 
 export default function Status404() {
     return (
         <div className="flex min-h-screen flex-col items-center justify-center">
-            <h1 className="mb-4 text-6xl font-bold text-gray-300">404</h1>
-            <p className="mb-8 text-xl text-gray-600">Nothing here yet</p>
-            <div className="border-2 border-solid">
-                <Link href="/" className="mb-8 px-4 py-2 text-gray-300">
-                    Get me to a safe place
-                </Link>
-            </div>
+            <h1 className="mb-4 text-6xl font-bold">404</h1>
+            <p className="mb-8 text-xl text-muted-foreground">Nothing here yet</p>
+            <Button variant="outline">
+                <Link href="/">{'Get me to a safe place'} </Link>
+            </Button>
         </div>
     );
 }
