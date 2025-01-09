a47aa65a8a64c6db19caabe114d785672b99e385
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-01-09 10:45:46 +0100

    Optimized select random scenario

--- app/play/random/page.tsx
+++ app/play/random/page.tsx
@@ -1,16 +1,11 @@
 import { ScenarioMap } from '~/components/scenario/scenario-map';
-import { redirect } from 'next/navigation';
-import { getScenarios } from '~/lib/db/queries';
+import { notFound } from 'next/navigation';
+import { getRandom } from '~/lib/db/queries';
 
 export default async function Page() {
-    const scenarios = await getScenarios();
+    const scenario = await getRandom();
 
-    const randomIndex = Math.floor(Math.random() * scenarios.length);
-
-    const scenario = scenarios.at(randomIndex);
-
-    // TODO: redirect to 404 instead
-    if (!scenario) redirect('/');
+    if (!scenario) notFound();
 
     return <ScenarioMap style={{ width: '100%', height: '100dvh' }} scenario={scenario.data} />;
 }
--- lib/db/queries.ts
+++ lib/db/queries.ts
@@ -1,4 +1,4 @@
-import { eq } from 'drizzle-orm';
+import { eq, sql } from 'drizzle-orm';
 import { db } from '.';
 import { ScenariosTable } from './schema';
 import { Scenario, scenarioSchema } from '~/lib/domain/scenario';
@@ -11,6 +11,19 @@ export const getScenarios = async () => {
 export const getScenario = async (id: number) => {
     const res = await db.query.ScenariosTable.findFirst({ where: (scenario, { eq }) => eq(scenario.id, id) });
     return res ? { ...res, data: scenarioSchema.parse(JSON.parse(res.data)) } : res;
+};
+
+export const getRandom = async () => {
+    const res = await db
+        .select()
+        .from(ScenariosTable)
+        .orderBy(sql`RANDOM()`)
+        .limit(1)
+        .execute();
+
+    const first = res.at(0);
+
+    return first ? { ...first, data: scenarioSchema.parse(JSON.parse(first.data)) } : first;
 };
 
 export const writeScenario = async (scenario: Scenario) => {
