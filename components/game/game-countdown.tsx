c89856119a36dcf602e75245a34def8e8c3ab558
Mario Prieto <mp@databeacon.aero>
2025-01-10 17:13:42 +0100

    Minor modifications

--- components/game/game-countdown.tsx
+++ components/game/game-countdown.tsx
@@ -2,22 +2,21 @@ import { useEffect, useState } from 'react';
 import { GAME_TIMEOUT_MS } from './game';
 
 const GameCountdown = ({ disabled }: { disabled?: boolean }) => {
-    const [counterMs, setCounterMs] = useState(GAME_TIMEOUT_MS);
+    const [counter, setCounter] = useState(GAME_TIMEOUT_MS / 1000);
 
     useEffect(() => {
         const tid = setTimeout(() => {
-            if (counterMs <= 0 || disabled) return;
-            setCounterMs((prev) => prev - 1000);
+            if (counter <= 0 || disabled) return;
+            setCounter((prev) => prev - 1);
+            console.log('counter', counter);
         }, 1000);
 
         return () => clearTimeout(tid);
-    }, [counterMs, disabled]);
-
-    const counter = counterMs / 1000;
+    }, [counter, disabled]);
 
     return (
         <div
-            className={`fixed right-16 top-5 z-10 flex items-center gap-1 rounded-md bg-white px-2.5 py-1 text-lg shadow-md ${counter < 10 ? 'text-red-500' : 'text-primary'}`}>
+            className={`fixed right-16 top-5 z-10 flex items-center gap-1 rounded-md bg-white px-2.5 py-1 text-base shadow-md transition-all hover:scale-110 ${counter < 10 ? 'text-red-500' : 'text-primary'} select-none`}>
             {`${counter} ${counter > 1 ? 'secs' : 'sec'}`}
         </div>
     );
