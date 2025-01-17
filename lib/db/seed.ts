324958a4970acf4bbe163306feae63d09f816046
Borja <bgm@databeacon.aero>
2025-01-17 10:42:39 +0100

    73 - Add json for scenarios seed

--- lib/db/seed.ts
+++ lib/db/seed.ts
@@ -1,9 +1,11 @@
 import { reset, seed } from 'drizzle-seed';
-import { UsersTable } from './schema';
 import { neon, neonConfig } from '@neondatabase/serverless';
 import ws from 'ws';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
+import sc1 from './scenarios-seed/scenario1.json';
+import sc2 from './scenarios-seed/scenario2.json';
+import sc3 from './scenarios-seed/scenario3.json';
 
 const connectionString = 'postgres://postgres:postgres@db.localtest.me:5432/main';
 
@@ -21,9 +23,24 @@ const sql = neon(connectionString);
 
 export const db = drizzle(sql, { schema });
 
+const { UsersTable, ScenariosTable } = schema;
+
 async function main() {
-    await reset(db, { UsersTable });
-    await seed(db, { UsersTable }, { count: 10 });
+    await reset(db, { UsersTable, ScenariosTable });
+    await seed(db, { UsersTable, ScenariosTable }).refine((f) => ({
+        UsersTable: {
+            count: 5
+        },
+        ScenariosTable: {
+            count: 3,
+            columns: {
+                data: f.valuesFromArray({
+                    isUnique: true,
+                    values: [JSON.stringify(sc1), JSON.stringify(sc2), JSON.stringify(sc3)]
+                })
+            }
+        }
+    }));
 }
 
 main();
