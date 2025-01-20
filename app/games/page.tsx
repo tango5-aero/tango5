f981005e054dcec64033d1a8ab0024c2621618e5
Mario Prieto <mp@databeacon.aero>
2025-01-20 10:10:03 +0100

    Action to allow a regular user reset his progress

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,6 +1,8 @@
 import { currentUser } from '@clerk/nextjs/server';
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
 import { Button } from '~/components/ui/button';
+import { UserResetAction } from '~/components/user/user-reset-action';
 import { UserGamesTable } from '~/components/users/usergames-table';
 import { getUnplayedScenarios, getUserGames } from '~/lib/db/queries';
 import { ScenarioSelect, UserGameSelect } from '~/lib/db/schema';
@@ -10,23 +12,29 @@ export default async function Page() {
 
     let userGames: UserGameSelect[] = [];
     let unplayedScenarios: ScenarioSelect[] = [];
-    if (user) {
-        userGames = await getUserGames(user.id);
-        unplayedScenarios = await getUnplayedScenarios(user.id);
+
+    if (!user) {
+        redirect('/');
     }
+
+    userGames = await getUserGames(user.id);
+    unplayedScenarios = await getUnplayedScenarios(user.id);
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-center gap-6 p-6 md:p-10">
             <h1 className="text-3xl">{'You have completed all available scenarios'}</h1>
             <h3 className="text-xl">{'Return later to access new scenarios'}</h3>
             <UserGamesTable usergames={userGames} />
-            <Button variant="outline">
-                {unplayedScenarios.length === 0 ? (
-                    <Link href="/play/random">Play random</Link>
-                ) : (
-                    <Link href="/play">Continue playing</Link>
-                )}
-            </Button>
+            <div className="flex flex-row gap-2">
+                <Button variant="outline">
+                    {unplayedScenarios.length === 0 ? (
+                        <Link href="/play/random">Play random</Link>
+                    ) : (
+                        <Link href="/play">Continue playing</Link>
+                    )}
+                </Button>
+                <UserResetAction id={user.id} />
+            </div>
         </main>
     );
 }
