6ffb8d06c89e8078773c7e15f079c7d6183a0b7e
Borja <bgm@databeacon.aero>
2025-01-21 17:54:01 +0100

    112 - Fix image with next

--- components/ui/welcome-tango5-title.tsx
+++ components/ui/welcome-tango5-title.tsx
@@ -3,9 +3,15 @@ import Image from 'next/image';
 const WelcomeTango5Title = () => {
     return (
         <div className="relative mb-14 flex h-44 flex-col items-end justify-between pl-40">
-            <Image src="/images/tango5-logo.svg" width="138" className="absolute bottom-0 left-0" alt="5" />
+            <Image
+                src="/images/tango5-logo.svg"
+                width="138"
+                height="171"
+                className="absolute bottom-0 left-0"
+                alt="5"
+            />
             <span className="font-BarlowLight text-7xl">Welcome to</span>
-            <Image src="/images/tango5.svg" width="355" alt="Tango5" />
+            <Image src="/images/tango5.svg" width="355" height="73" alt="Tango5" />
         </div>
     );
 };
