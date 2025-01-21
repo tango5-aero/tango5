332c4bcf6d8c28356079ab3f826b56738e1c855c
Borja <bgm@databeacon.aero>
2025-01-21 13:47:15 +0100

    112 - Add video background

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,21 @@ export default async function Page() {
     }
 
     return (
-        <main className="flex h-screen flex-col items-center justify-center gap-6 p-6 md:p-10">
-            <h1 className="text-3xl">{'Welcome to Tango5'}</h1>
-            <SignedOut>
-                <SignIn routing="hash" />
-            </SignedOut>
-        </main>
+        <>
+            <main className="relative z-20 flex h-screen flex-col items-center justify-center gap-6 p-6 md:p-10">
+                <h1 className="text-3xl">{'Welcome to Tango5'}</h1>
+                <SignedOut>
+                    <SignIn routing="hash" />
+                </SignedOut>
+            </main>
+            <div className="absolute left-0 top-0 z-10 h-full w-full bg-[#20282E] mix-blend-color"></div>
+            <video
+                autoPlay
+                loop
+                muted
+                className="absolute left-0 top-0 z-0 h-full w-full object-cover opacity-70"
+                src="./video/databeacon-bg.mp4"></video>
+            <div className="absolute left-0 top-0 z-0 h-full w-full bg-[#011B26] opacity-50"></div>
+        </>
     );
 }
