792ca8a1c05509484ae80db97df09672db4dd29b
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-01-21 14:22:50 +0100

    Missing primary map value

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
                 ghost: 'hover:bg-accent hover:text-accent-foreground',
                 link: 'text-primary underline-offset-4 hover:underline',
                 default_map:
-                    'bg-primary text-2xl text-primary-foreground--map font-bold shadow hover:bg-primary--map/90 rounded-full'
+                    'bg-primary--map text-2xl text-primary-foreground--map font-bold shadow hover:bg-primary--map/90 rounded-full'
             },
             size: {
                 default: 'h-9 px-4 py-2',
