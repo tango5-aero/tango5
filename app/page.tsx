091abb61f1efb15451e826f1371ef352b521b2e0
Borja <bgm@databeacon.aero>
2025-01-21 14:52:56 +0100

    112 - Add video background

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { SignedOut, SignIn } from '@clerk/nextjs';
 import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
+import { VideoBackground } from '~/components/ui/video-background';
 import { tryCreateUser } from '~/lib/db/queries';
 
 export default async function Page() {
@@ -19,14 +20,7 @@ export default async function Page() {
                     <SignIn routing="hash" />
                 </SignedOut>
             </main>
-            <div className="absolute left-0 top-0 z-10 h-full w-full bg-[#20282E] mix-blend-color"></div>
-            <video
-                autoPlay
-                loop
-                muted
-                className="absolute left-0 top-0 z-0 h-full w-full object-cover opacity-70"
-                src="./video/databeacon-bg.mp4"></video>
-            <div className="absolute left-0 top-0 z-0 h-full w-full bg-[#011B26] opacity-50"></div>
+            <VideoBackground />
         </>
     );
 }
