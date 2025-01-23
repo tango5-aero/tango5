809f1d00fd32c190a540480cba3ae341572f1eb5
Mario Prieto <mp@databeacon.aero>
2025-01-23 13:04:30 +0100

    Add a generic component and a custom hook for current dialogs that trigger a server action

--- components/scenario/scenario-upload-dialog.tsx
+++ components/scenario/scenario-upload-dialog.tsx
@@ -1,44 +1,30 @@
 'use client';
 
-import { startTransition, useActionState, useEffect, useState } from 'react';
-import { Button } from '~/components/ui/button';
+import { startTransition, useEffect, useState } from 'react';
 import { Input } from '~/components/ui/input';
-import revalidateCacheTag, { createScenario } from '~/lib/actions';
-import { toast } from 'sonner';
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-    DialogClose
-} from '~/components/ui/dialog';
+import { createScenario } from '~/lib/actions';
 import { FilePlus2 } from 'lucide-react';
 import { cacheTags } from '~/lib/constants';
+import { ActionDialog } from '../ui/action-dialog';
+import { useDialogAction } from '~/hooks/use-dialog-action';
+import { Button } from '../ui/button';
 
 export function ScenarioUploadDialog() {
     const [open, setOpen] = useState(false);
-    const [state, action, pending] = useActionState(createScenario, { message: '', error: false });
     const [data, setData] = useState('');
     const [fileName, setFileName] = useState('');
 
-    useEffect(() => {
-        if (state.message && state.error) toast.error(state.message);
-        if (state.message && !state.error) toast.success(state.message);
-        revalidateCacheTag(cacheTags.scenarios);
-    }, [state]);
+    const { action, pending } = useDialogAction(
+        `Creating new scenario from ${fileName}`,
+        createScenario,
+        cacheTags.scenarios
+    );
 
     useEffect(() => {
         if (!open) setData('');
     }, [open]);
 
-    useEffect(() => {
-        if (pending) toast.info(`Creating new scenario from ${fileName}`);
-    }, [fileName, pending]);
-
-    const uploadScenario = (data: string, fileName: string) => {
+    const handleConfirm = () => {
         if (data)
             startTransition(async () => {
                 action({ data, fileName });
@@ -54,35 +40,25 @@ export function ScenarioUploadDialog() {
     };
 
     return (
-        <Dialog open={open} onOpenChange={setOpen}>
-            <DialogTrigger asChild>
+        <ActionDialog
+            open={open}
+            openHandler={setOpen}
+            title={'Scenario Upload'}
+            pending={pending}
+            triggerAsChild
+            dialogTrigger={
                 <Button variant={'outline'}>
                     <FilePlus2 size={'1rem'} /> {'New'}
                 </Button>
-            </DialogTrigger>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>{'Scenario Upload'}</DialogTitle>
-                    <DialogDescription>{'Create a new scenario from a JSON file'}</DialogDescription>
-                </DialogHeader>
-
-                <Input
-                    type="file"
-                    onChange={({ target: { files } }) => {
-                        const file = files?.item(0);
-                        if (file) updateFileContents(file);
-                    }}
-                />
-
-                <DialogFooter>
-                    <Button disabled={data === '' || pending} onClick={() => uploadScenario(data, fileName)}>
-                        {pending ? 'Loading' : 'Submit'}
-                    </Button>
-                    <DialogClose asChild>
-                        <Button variant="outline">{'Cancel'}</Button>
-                    </DialogClose>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
+            }
+            onConfirm={handleConfirm}>
+            <Input
+                type="file"
+                onChange={({ target: { files } }) => {
+                    const file = files?.item(0);
+                    if (file) updateFileContents(file);
+                }}
+            />
+        </ActionDialog>
     );
 }
