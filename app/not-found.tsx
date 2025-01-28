bb93a0b4bb54ace1da51fb1d86fc427ecc370a91
Mario Prieto <mp@databeacon.aero>
2025-01-28 11:04:35 +0100

    Fix buttons with link

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,9 +6,9 @@ export default function Status404() {
         <div className="flex min-h-screen flex-col items-center justify-center">
             <h1 className="mb-4 text-6xl font-bold">404</h1>
             <p className="mb-8 text-xl text-muted-foreground">Nothing here yet</p>
-            <Button variant="outline">
-                <Link href="/">{'Get me to a safe place'} </Link>
-            </Button>
+            <Link href="/" passHref>
+                <Button variant="outline">{'Get me to a safe place'} </Button>
+            </Link>
         </div>
     );
 }
