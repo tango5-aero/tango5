9b888c40d5bb2c99e5d7892f95536f36821d0d54
Mario Prieto <mp@databeacon.aero>
2025-01-28 10:27:07 +0100

    Add button to leave the game

--- app/(game)/play/[scenarioId]/page.tsx
+++ app/(game)/play/[scenarioId]/page.tsx
@@ -1,6 +1,9 @@
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
+import { LogOutIcon } from 'lucide-react';
 import { getScenario } from '~/lib/db/queries';
 import { Game } from '~/components/game/game';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '~/components/ui/tooltip';
 
 export default async function Page({ params }: { params: Promise<{ scenarioId: number }> }) {
     const id = (await params).scenarioId;
@@ -10,5 +13,21 @@ export default async function Page({ params }: { params: Promise<{ scenarioId: n
 
     if (!scenario?.data) notFound();
 
-    return <Game id={id} scenarioData={scenario.data} nextUrl={'/play'} />;
+    return (
+        <>
+            <TooltipProvider delayDuration={0}>
+                <Tooltip>
+                    <Link href="/games">
+                        <TooltipTrigger asChild>
+                            <LogOutIcon className="fixed right-16 top-5 z-10 cursor-pointer text-white" />
+                        </TooltipTrigger>
+                    </Link>
+                    <TooltipContent side="bottom" sideOffset={10} align="center">
+                        <span className="text-sm text-white">{'Leave game'}</span>
+                    </TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
+            <Game id={id} scenarioData={scenario.data} nextUrl={'/play'} />
+        </>
+    );
 }
