efdd89cb43715f396a70e5672542b041fe7bcbda
Borja <bgm@databeacon.aero>
2025-01-30 10:29:56 +0100

    200 - Use defaultChecked

--- components/user/user-notify-me-form.tsx
+++ components/user/user-notify-me-form.tsx
@@ -1,21 +1,23 @@
 'use client';
 
-import { PropsWithoutRef, useState } from 'react';
+import { PropsWithoutRef } from 'react';
 import { updateConsent } from '~/lib/actions/users';
 
 const UserNotifyMeForm = (props: PropsWithoutRef<{ consent: boolean }>) => {
-    const [consent, setConsent] = useState(props.consent);
-
     const onChangeConsent = (e: React.ChangeEvent<HTMLInputElement>) => {
         const consent = e.target.checked;
-        setConsent(consent);
         updateConsent(consent);
     };
 
     return (
         <div className="flex flex-row gap-5">
             <label className="hover:cursor-pointer">
-                <input className="hover:cursor-pointer" type="checkbox" checked={consent} onChange={onChangeConsent} />{' '}
+                <input
+                    className="hover:cursor-pointer"
+                    type="checkbox"
+                    defaultChecked={props.consent}
+                    onChange={onChangeConsent}
+                />{' '}
                 {'Notify me on updates and new scenarios'}
             </label>
         </div>
