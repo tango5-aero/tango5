1179e93b1f56919461e8c8b5adc17953378e9dc2
Borja <bgm@databeacon.aero>
2025-02-04 13:47:10 +0100

    216 - First approach to carousel

--- app/tutorial/page.tsx
+++ app/tutorial/page.tsx
@@ -12,9 +12,17 @@ export default async function Page() {
                     <UserButton />
                 </SignedIn>
             </div>
-            <main className="relative z-20 flex h-screen flex-col items-center justify-center gap-10 p-6 md:p-10">
+            <main className="relative z-20 flex flex-col items-center justify-center gap-10 p-6 md:p-10">
                 <WelcomeTango5Title />
                 <TutorialCarousel />
+                <h2 className="font-BarlowBold text-4xl">FAQ</h2>
+                <details className="w-full rounded-lg border border-map p-3">
+                    <summary className="font-BarlowLight text-xl hover:cursor-pointer">What is Tango5?</summary>
+                    <div className="mt-4 font-BarlowLight">
+                        Tango5 (or T5 for short) is a suite of tools designed to enhance and challenge your Air Traffic
+                        Control (ATC) skills.{' '}
+                    </div>
+                </details>
                 <Link
                     href="/play"
                     className="h-12 rounded-full bg-map px-8 py-2 font-BarlowBold text-2xl font-bold leading-7 text-map-foreground shadow hover:bg-map/85">
--- components/ui/tutorial-carousel.tsx
+++ components/ui/tutorial-carousel.tsx
@@ -1,12 +1,13 @@
 'use client';
+import Image from 'next/image';
 import { Carousel } from 'nuka-carousel';
 
 const TutorialCarousel = () => {
     return (
-        <Carousel showDots>
-            <div>Slide 1</div>
-            <div>Slide 2</div>
-            <div>Slide 3</div>
+        <Carousel showDots showArrows>
+            <Image src="/images/tutorial/tutorial1.png" alt="Tutorial 1" width={1920} height={1080} />
+            <Image src="/images/tutorial/tutorial2.png" alt="Tutorial 2" width={1920} height={1080} />
+            <Image src="/images/tutorial/tutorial3.png" alt="Tutorial 3" width={1920} height={1080} />
         </Carousel>
     );
 };
