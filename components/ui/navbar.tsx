3c0b5c92f1f048cf15545ff24cf98e8947a9f458
Borja <bgm@databeacon.aero>
2025-02-06 10:05:46 +0100

    243 - Styles for navbar

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,42 +1,67 @@
+'use client';
+
 import { SignedIn, SignedOut, SignIn, UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
-import Link from 'next/link';
+import { LinkButton } from './link-button';
+import { useParams, usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
+    const pathname = usePathname();
+    const params = useParams();
+    const [anchor, setAnchor] = useState('');
+
+    useEffect(() => {
+        setAnchor(window.location.hash);
+    }, [params, window.location.hash]);
+
     return (
         <nav className="relative z-30">
-            <div className="bg-gray-800 p-4">
-                <div className="container mx-auto">
-                    <div className="flex items-center justify-between">
-                        <div>
-                            <Image src="/images/tango5.svg" width={120} height={30} alt={'Tango5'} />
-                        </div>
-                        <div className="flex flex-row gap-4">
-                            <Link href="/" className="text-white">
-                                Tango5
-                            </Link>
-                            <Link href="/app/tutorial" className="text-white">
-                                Tutorial
-                            </Link>
-                            <Link href="/app/tutorial#faq" className="text-white">
-                                FAQ
-                            </Link>
-                            <Link href="/app/scores" className="text-white">
-                                Scores
-                            </Link>
-                            <Link href="/app/play" className="text-white">
-                                Play
-                            </Link>
-                            <Link href="/backstage" className="text-white">
-                                Backstage
-                            </Link>
-                            <SignedOut>
-                                <SignIn routing="hash" />
-                            </SignedOut>
-                            <SignedIn>
-                                <UserButton />
-                            </SignedIn>
-                        </div>
+            <div className="flex h-[130px] flex-col justify-center bg-navbarBG">
+                <div className="flex items-center justify-between px-12 text-xl">
+                    <div>
+                        <Image src="/images/tango5-logo.svg" width={30} height={37} alt={'Tango5'} />
+                    </div>
+                    <div className="flex flex-row">
+                        <LinkButton
+                            href="/"
+                            className={`text-xl text-white ${pathname === '/' ? 'font-BarlowBold' : 'font-BarlowLight'}`}
+                            variant="link">
+                            Tango5
+                        </LinkButton>
+                        <LinkButton
+                            href="/app/tutorial"
+                            className={`text-xl text-white ${pathname === '/app/tutorial' && anchor !== '#faq' ? 'font-BarlowBold' : 'font-BarlowLight'}`}
+                            variant="link">
+                            Tutorial
+                        </LinkButton>
+                        <LinkButton
+                            href="/app/tutorial#faq"
+                            className={`text-xl text-white ${pathname === '/app/tutorial' && anchor === '#faq' ? 'font-BarlowBold' : 'font-BarlowLight'}`}
+                            variant="link">
+                            FAQ
+                        </LinkButton>
+                        <LinkButton
+                            href="/app/scores"
+                            className={`text-xl text-white ${pathname === '/app/scores' ? 'font-BarlowBold' : 'font-BarlowLight'}`}
+                            variant="link">
+                            Scores
+                        </LinkButton>
+                        <LinkButton href="/app/play" className="text-xl" variant="map">
+                            Play
+                        </LinkButton>
+                        <LinkButton
+                            href="/backstage"
+                            className={`text-xl text-white ${pathname === '/backstage' ? 'font-BarlowBold' : 'font-BarlowLight'}`}
+                            variant="link">
+                            Backstage
+                        </LinkButton>
+                        <SignedOut>
+                            <SignIn routing="hash" />
+                        </SignedOut>
+                        <SignedIn>
+                            <UserButton />
+                        </SignedIn>
                     </div>
                 </div>
             </div>
--- components/ui/video-background.tsx
+++ components/ui/video-background.tsx
@@ -7,7 +7,7 @@ const VideoBackground = () => {
                 loop
                 muted
                 className="absolute left-0 top-0 z-0 h-full w-full object-cover opacity-80"
-                src="./video/databeacon-bg.mp4"></video>
+                src="/video/databeacon-bg.mp4"></video>
             <div className="absolute left-0 top-0 z-0 h-full w-full bg-[#011B26] opacity-50"></div>
         </>
     );
