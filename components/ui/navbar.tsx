a5600ac9cab969f8f6a4714198bae2f2079397f8
Borja <bgm@databeacon.aero>
2025-02-10 11:55:46 +0100

    243 - Refactor links navbar

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -3,18 +3,12 @@
 import { SignedIn, SignedOut, SignIn, UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 import { LinkButton } from './link-button';
-import { useParams, usePathname } from 'next/navigation';
-import { PropsWithoutRef, useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
+import { PropsWithoutRef } from 'react';
 import Link from 'next/link';
 
 const Navbar = (props: PropsWithoutRef<{ backstageAccess: boolean }>) => {
     const pathname = usePathname();
-    const params = useParams();
-    const [anchor, setAnchor] = useState('');
-
-    useEffect(() => {
-        setAnchor(window.location.hash);
-    }, [params]);
 
     return (
         <nav className="fixed z-30 w-full">
@@ -26,17 +20,19 @@ const Navbar = (props: PropsWithoutRef<{ backstageAccess: boolean }>) => {
                         </Link>
                     </div>
                     <div className="flex flex-row gap-1">
+                        {props.backstageAccess && (
+                            <LinkButton
+                                href="/backstage"
+                                className={`text-xl text-white ${pathname.includes('/backstage') ? 'font-BarlowBold' : 'font-BarlowLight'}`}
+                                variant="link">
+                                Backstage
+                            </LinkButton>
+                        )}
                         <LinkButton
                             href="/app/tutorial"
-                            className={`text-xl text-white ${pathname === '/app/tutorial' && anchor !== '#faq' ? 'font-BarlowBold' : 'font-BarlowLight'}`}
+                            className={`text-xl text-white ${pathname === '/app/tutorial' ? 'font-BarlowBold' : 'font-BarlowLight'}`}
                             variant="link">
                             Tutorial
-                        </LinkButton>
-                        <LinkButton
-                            href="/app/tutorial#faq"
-                            className={`text-xl text-white ${pathname === '/app/tutorial' && anchor === '#faq' ? 'font-BarlowBold' : 'font-BarlowLight'}`}
-                            variant="link">
-                            FAQ
                         </LinkButton>
                         <LinkButton
                             href="/app/scores"
@@ -47,14 +43,6 @@ const Navbar = (props: PropsWithoutRef<{ backstageAccess: boolean }>) => {
                         <LinkButton href="/app/play" className="text-xl" variant="map">
                             Play
                         </LinkButton>
-                        {props.backstageAccess && (
-                            <LinkButton
-                                href="/backstage"
-                                className={`text-xl text-white ${pathname.includes('/backstage') ? 'font-BarlowBold' : 'font-BarlowLight'}`}
-                                variant="link">
-                                Backstage
-                            </LinkButton>
-                        )}
                         <SignedOut>
                             <SignIn routing="hash" />
                         </SignedOut>
